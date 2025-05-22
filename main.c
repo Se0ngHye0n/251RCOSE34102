94f90b96647338c12aa0185cb5f783bec421e8d8
SeongHyeon Mun <tjdgus2542@naver.com>
2025-05-22 23:59:58 +0900

    add non-preemptive SJF, non-preemptive Priority, RR, IO mark in Gantt chart

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 #include <time.h>
 
 
-#define MAX_PROCESS_COUNT 10
-#define MAX_TIME_UNITS 200
+#define PROCESS_COUNT 5
+#define QUEUE_SIZE (PROCESS_COUNT + 1)
+#define MAX_TIME_UNITS 100
+#define TIME_QUANTUM 4
 
 
 typedef struct {
@@ -23,42 +25,64 @@ typedef struct {
     int executed_time;
 } Process;
 
-Process process_list[MAX_PROCESS_COUNT];
-int process_count = MAX_PROCESS_COUNT;
+Process process_list[PROCESS_COUNT];
+int process_count = PROCESS_COUNT;
 
-Process* ready_queue[MAX_PROCESS_COUNT];
+Process* ready_queue[QUEUE_SIZE];
 int ready_front = 0;
 int ready_rear = 0;
-Process* waiting_queue[MAX_PROCESS_COUNT];
+
+Process* waiting_queue[QUEUE_SIZE];
 int waiting_front = 0;
 int waiting_rear = 0;
 
-int timeline[MAX_TIME_UNITS];
-int timeline_end = 0;
+int gantt[MAX_TIME_UNITS];
+int gantt_io[MAX_TIME_UNITS];
+int gantt_end = 0;
 
 
 void create_process();
 void print_process_list();
 void initialization();
 void scheduling_FCFS();
+void scheduling_Non_Preemptive_SJF();
+void scheduling_Non_Preemptive_Priority();
+void scheduling_Round_Robin();
 void print_gantt();
 void evaluation();
 
 
 int main(void) {
-    srand((unsigned)time(NULL));
+    srand(time(NULL));
 
     create_process();
 
     print_process_list();
 
     initialization();
-
-    printf("\n=== FCFS Scheduling ===\n");
+    printf("\nFCFS Scheduling\n");
     scheduling_FCFS();
-
     print_gantt();
     evaluation();
+
+    initialization();
+    printf("\nNon-Preemptive SJF Scheduling\n");
+    scheduling_Non_Preemptive_SJF();
+    print_gantt();
+    evaluation();
+
+    initialization();
+    printf("\nNon-Preemptive Priority Scheduling\n");
+    scheduling_Non_Preemptive_Priority();
+    print_gantt();
+    evaluation();
+
+    initialization();
+    printf("\nRound Robin Scheduling\n");
+    scheduling_Round_Robin();
+    print_gantt();
+    evaluation();
+
     return 0;
 }
 
@@ -69,9 +93,9 @@ void create_process() {
         p->pid = i + 1;
         p->priority = rand() % 5;
         p->arrival_time = rand() % 10;
-        p->cpu_burst = (rand() % 10) + 1;
+        p->cpu_burst = (rand() % 9) + 2;
         p->io_burst = (rand() % 5) + 1;
-        p->io_request_time = rand() % p->cpu_burst;
+        p->io_request_time = (rand() % (p->cpu_burst - 1)) + 1;
         p->start_time = -1;
         p->completion_time = 0;
         p->waiting_time = 0;
@@ -103,7 +127,21 @@ void initialization() {
     waiting_rear = 0;
 
     for (int i = 0; i < MAX_TIME_UNITS; i++) {
-        timeline[i] = 0;
+        gantt[i] = 0;
+        gantt_io[i] = 0;
+    }
+
+    gantt_end = 0;
+
+    for (int i = 0; i < process_count; i++) {
+        Process* p = &process_list[i];
+        p->remaining_cpu = p->cpu_burst;
+        p->remaining_io = 0;
+        p->executed_time = 0;
+        p->start_time = -1;
+        p->completion_time = 0;
+        p->waiting_time = 0;
+        p->turnaround_time = 0;
     }
 }
 
@@ -112,35 +150,38 @@ void scheduling_FCFS() {
     int completed_process_count = 0;
     Process* current = NULL;
     int idle = 1;
-    
+
     while (completed_process_count < process_count && time < MAX_TIME_UNITS) {
+        gantt[time] = 0;
+        gantt_io[time] = 0;
+
         for (int i = 0; i < process_count; i++) {
             Process* p = &process_list[i];
             if (p->arrival_time == time) {
                 ready_queue[ready_rear] = p;
-                if (++ready_rear == MAX_PROCESS_COUNT) {
+                if (++ready_rear == QUEUE_SIZE) {
                     ready_rear = 0;
                 }
             }
         }
 
-        int len = (waiting_rear - waiting_front + MAX_PROCESS_COUNT) % MAX_PROCESS_COUNT;
+        int len = (waiting_rear - waiting_front + QUEUE_SIZE) % QUEUE_SIZE;
 
         for (int i = 0; i < len; i++) {
             Process* p = waiting_queue[waiting_front];
-            if (++waiting_front == MAX_PROCESS_COUNT) {
+            if (++waiting_front == QUEUE_SIZE) {
                 waiting_front = 0;
             }
             p->remaining_io--;
             if (p->remaining_io > 0) {
                 waiting_queue[waiting_rear] = p;
-                if (++waiting_rear == MAX_PROCESS_COUNT) {
+                if (++waiting_rear == QUEUE_SIZE) {
                     waiting_rear = 0;
                 }
             }
             else {
                 ready_queue[ready_rear] = p;
-                if (++ready_rear == MAX_PROCESS_COUNT) {
+                if (++ready_rear == QUEUE_SIZE) {
                     ready_rear = 0;
                 }
             }
@@ -149,12 +190,13 @@ void scheduling_FCFS() {
         if (!current) {
             if (ready_front != ready_rear) {
                 current = ready_queue[ready_front];
-                if (++ready_front == MAX_PROCESS_COUNT) {
+                if (++ready_front == QUEUE_SIZE) {
                     ready_front = 0;
                 }
                 if (current->start_time < 0) {
                     current->start_time = time;
                 }
+
                 idle = 0;
             }
             else {
@@ -165,14 +207,15 @@ void scheduling_FCFS() {
         if (current) {
             current->executed_time++;
             current->remaining_cpu--;
-            timeline[time] = current->pid;
+            gantt[time] = current->pid;
             if (current->executed_time == current->io_request_time) {
                 current->remaining_io = current->io_burst;
                 waiting_queue[waiting_rear] = current;
-                if (++waiting_rear == MAX_PROCESS_COUNT) {
+                if (++waiting_rear == QUEUE_SIZE) {
                     waiting_rear = 0;
                 }
                 current = NULL;
+                gantt_io[time] = 1;
                 idle = 0;
             }
             else if (current->remaining_cpu == 0) {
@@ -184,44 +227,397 @@ void scheduling_FCFS() {
         }
 
         if (idle) {
-            timeline[time] = 0;
+            gantt[time] = 0;
         }
+
         time++;
     }
-    timeline_end = time;
+
+    gantt_end = time;
 
     for (int i = 0; i < process_count; i++) {
         Process* p = &process_list[i];
         p->turnaround_time = p->completion_time - p->arrival_time;
-        p->waiting_time = p->turnaround_time - p->cpu_burst;
+        p->waiting_time = p->start_time - p->arrival_time;
+    }
+}
+
+void scheduling_Non_Preemptive_SJF() {
+    int time = 0;
+    int completed_count = 0;
+    Process* current = NULL;
+    int idle = 1;
+
+    while (completed_count < process_count && time < MAX_TIME_UNITS) {
+        gantt[time] = 0;
+        gantt_io[time] = 0;
+
+        for (int i = 0; i < process_count; i++) {
+            Process* p = &process_list[i];
+            if (p->arrival_time == time) {
+                ready_queue[ready_rear] = p;
+                if (++ready_rear == QUEUE_SIZE) {
+                    ready_rear = 0;
+                }
+            }
+        }
+
+        int len = (waiting_rear - waiting_front + QUEUE_SIZE) % QUEUE_SIZE;
+        for (int i = 0; i < len; i++) {
+            Process* p = waiting_queue[waiting_front];
+            if (++waiting_front == QUEUE_SIZE) {
+                waiting_front = 0;
+            }
+            if (--p->remaining_io > 0) {
+                waiting_queue[waiting_rear] = p;
+                if (++waiting_rear == QUEUE_SIZE) {
+                    waiting_rear = 0;
+                }
+            }
+            else {
+                ready_queue[ready_rear] = p;
+                if (++ready_rear == QUEUE_SIZE) {
+                    ready_rear = 0;
+                }
+            }
+        }
+
+        if (!current) {
+            if (ready_front != ready_rear) {
+                int idx = ready_front;
+                int shortest_process = idx;
+                int min_cpu_burst = ready_queue[idx]->remaining_cpu;
+
+                int count = (ready_rear - ready_front + QUEUE_SIZE) % QUEUE_SIZE;
+                for (int k = 0; k < count; k++) {
+                    Process* p = ready_queue[(ready_front + k) % QUEUE_SIZE];
+                    if (p->remaining_cpu < min_cpu_burst) {
+                        min_cpu_burst = p->remaining_cpu;
+                        shortest_process = (ready_front + k) % QUEUE_SIZE;
+                    }
+                }
+
+                current = ready_queue[shortest_process];
+
+                for (int k = shortest_process; k != ready_rear; k = (k + 1) % QUEUE_SIZE) {
+                    int next = (k + 1) % QUEUE_SIZE;
+                    ready_queue[k] = ready_queue[next];
+                }
+
+                if (ready_rear == 0) {
+                    ready_rear = QUEUE_SIZE - 1;
+                }
+                else {
+                    ready_rear--;
+                }
+
+                if (current->start_time < 0) {
+                    current->start_time = time;
+                }
+
+                idle = 0;
+            }
+            else {
+                idle = 1;
+            }
+        }
+
+        if (current) {
+            gantt[time] = current->pid;
+            current->executed_time++;
+            current->remaining_cpu--;
+            if (current->executed_time == current->io_request_time) {
+                current->remaining_io = current->io_burst;
+                waiting_queue[waiting_rear] = current;
+                if (++waiting_rear == QUEUE_SIZE) {
+                    waiting_rear = 0;
+                }
+                current = NULL;
+                gantt_io[time] = 1;
+                idle = 0;
+            }
+            else if (current->remaining_cpu == 0) {
+                current->completion_time = time + 1;
+                completed_count++;
+                current = NULL;
+                idle = 0;
+            }
+        }
+
+        if (idle) {
+            gantt[time] = 0;
+        }
+
+        time++;
+    }
+
+    gantt_end = time;
+
+    for (int i = 0; i < process_count; i++) {
+        Process* p = &process_list[i];
+        p->turnaround_time = p->completion_time - p->arrival_time;
+        p->waiting_time = p->start_time - p->arrival_time;
+    }
+}
+
+void scheduling_Non_Preemptive_Priority() {
+    int time = 0;
+    int completed_count = 0;
+    Process* current = NULL;
+    int idle = 1;
+
+    while (completed_count < process_count && time < MAX_TIME_UNITS) {
+        gantt[time] = 0;
+        gantt_io[time] = 0;
+
+        for (int i = 0; i < process_count; i++) {
+            Process* p = &process_list[i];
+            if (p->arrival_time == time) {
+                ready_queue[ready_rear] = p;
+                if (++ready_rear == QUEUE_SIZE) {
+                    ready_rear = 0;
+                }
+            }
+        }
+
+        int len = (waiting_rear - waiting_front + QUEUE_SIZE) % QUEUE_SIZE;
+        for (int i = 0; i < len; i++) {
+            Process* p = waiting_queue[waiting_front];
+            if (++waiting_front == QUEUE_SIZE) {
+                waiting_front = 0;
+            }
+            if (--p->remaining_io > 0) {
+                waiting_queue[waiting_rear] = p;
+                if (++waiting_rear == QUEUE_SIZE) {
+                    waiting_rear = 0;
+                }
+            }
+            else {
+                ready_queue[ready_rear] = p;
+                if (++ready_rear == QUEUE_SIZE) {
+                    ready_rear = 0;
+                }
+            }
+        }
+
+        if (!current) {
+            if (ready_front != ready_rear) {
+                int idx = ready_front;
+                int selected_process = idx;
+                int best_priority = ready_queue[idx]->priority;
+
+                int count = (ready_rear - ready_front + QUEUE_SIZE) % QUEUE_SIZE;
+                for (int k = 0; k < count; k++) {
+                    Process* p = ready_queue[(ready_front + k) % QUEUE_SIZE];
+                    if (p->priority > best_priority) {
+                        best_priority = p->priority;
+                        selected_process = (ready_front + k) % QUEUE_SIZE;
+                    }
+                }
+
+                current = ready_queue[selected_process];
+
+                for (int k = selected_process; k != ready_rear; k = (k + 1) % QUEUE_SIZE) {
+                    int next = (k + 1) % QUEUE_SIZE;
+                    ready_queue[k] = ready_queue[next];
+                }
+
+                if (ready_rear == 0) {
+                    ready_rear = QUEUE_SIZE - 1;
+                }
+                else {
+                    ready_rear--;
+                }
+
+                if (current->start_time < 0) {
+                    current->start_time = time;
+                }
+
+                idle = 0;
+            }
+            else {
+                idle = 1;
+            }
+        }
+
+        if (current) {
+            gantt[time] = current->pid;
+            current->executed_time++;
+            current->remaining_cpu--;
+            if (current->executed_time == current->io_request_time) {
+                current->remaining_io = current->io_burst;
+                waiting_queue[waiting_rear] = current;
+                if (++waiting_rear == QUEUE_SIZE) {
+                    waiting_rear = 0;
+                }
+                current = NULL;
+                gantt_io[time] = 1;
+                idle = 0;
+            }
+            else if (current->remaining_cpu == 0) {
+                current->completion_time = time + 1;
+                completed_count++;
+                current = NULL;
+                idle = 0;
+            }
+        }
+
+        if (idle) {
+            gantt[time] = 0;
+        }
+
+        time++;
+    }
+
+    gantt_end = time;
+
+    for (int i = 0; i < process_count; i++) {
+        Process* p = &process_list[i];
+        p->turnaround_time = p->completion_time - p->arrival_time;
+        p->waiting_time = p->start_time - p->arrival_time;
+    }
+}
+
+void scheduling_Round_Robin() {
+    int time = 0;
+    int completed_count = 0;
+    Process* current = NULL;
+    int idle = 1;
+    int quantum_count = 0;
+
+    while (completed_count < process_count && time < MAX_TIME_UNITS) {
+        gantt[time] = 0;
+        gantt_io[time] = 0;
+
+        for (int i = 0; i < process_count; i++) {
+            Process* p = &process_list[i];
+            if (p->arrival_time == time) {
+                ready_queue[ready_rear] = p;
+                if (++ready_rear == QUEUE_SIZE) {
+                    ready_rear = 0;
+                }
+            }
+        }
+
+        int len = (waiting_rear - waiting_front + QUEUE_SIZE) % QUEUE_SIZE;
+        for (int i = 0; i < len; i++) {
+            Process* p = waiting_queue[waiting_front];
+            if (++waiting_front == QUEUE_SIZE) {
+                waiting_front = 0;
+            }
+            if (--p->remaining_io > 0) {
+                waiting_queue[waiting_rear] = p;
+                if (++waiting_rear == QUEUE_SIZE) {
+                    waiting_rear = 0;
+                }
+            }
+            else {
+                ready_queue[ready_rear] = p;
+                if (++ready_rear == QUEUE_SIZE) {
+                    ready_rear = 0;
+                }
+            }
+        }
+
+        if (!current || quantum_count == TIME_QUANTUM) {
+            if (current) {
+                ready_queue[ready_rear] = current;
+                if (++ready_rear == QUEUE_SIZE) {
+                    ready_rear = 0;
+                }
+                current = NULL;
+            }
+
+            if (ready_front != ready_rear) {
+                current = ready_queue[ready_front];
+                if (++ready_front == QUEUE_SIZE) {
+                    ready_front = 0;
+                }
+                if (current->start_time < 0) {
+                    current->start_time = time;
+                }
+
+                quantum_count = 0;
+                idle = 0;
+            }
+            else {
+                idle = 1;
+            }
+        }
+
+        if (current) {
+            gantt[time] = current->pid;
+            current->executed_time++;
+            current->remaining_cpu--;
+            quantum_count++;
+
+            if (current->executed_time == current->io_request_time) {
+                current->remaining_io = current->io_burst;
+                waiting_queue[waiting_rear] = current;
+                if (++waiting_rear == QUEUE_SIZE) {
+                    waiting_rear = 0;
+                }
+
+                current = NULL;
+                gantt_io[time] = 1;
+                idle = 0;
+                quantum_count = 0;
+            }
+            else if (current->remaining_cpu == 0) {
+                current->completion_time = time + 1;
+                completed_count++;
+                current = NULL;
+                idle = 0;
+                quantum_count = 0;
+            }
+        }
+
+        if (idle) {
+            gantt[time] = 0;
+        }
+
+        time++;
+    }
+
+    gantt_end = time;
+
+    for (int i = 0; i < process_count; i++) {
+        Process* p = &process_list[i];
+        p->turnaround_time = p->completion_time - p->arrival_time;
+        p->waiting_time = p->start_time - p->arrival_time;
     }
 }
 
 void print_gantt() {
-    const int width = 5;
+    const int width = 10;
 
     printf("\nGantt Chart:\nTime :");
 
-    for (int t = 0; t <= timeline_end; t++) {
+    for (int t = 0; t <= gantt_end; t++) {
         printf("%*d", width, t);
-}
+    }
 
     printf("\n ");
-        for (int t = 0; t <= timeline_end; t++) {
-            for (int i = 0; i < width; i++) printf("-");
-        }
+    for (int t = 0; t <= gantt_end; t++) {
+        for (int i = 0; i < width; i++) printf("-");
+    }
 
     printf("\nPID  :");
-        for (int t = 0; t <= timeline_end; t++) {
-            if (timeline[t] == 0) {
-                printf("|%-*s", width - 1, "Idle");
-            }
-            else {
-                char buffer[width + 1];
-                snprintf(buffer, sizeof(buffer), "P%d", timeline[t]);
-                printf("| %*s", width - 2, buffer);
-            }
+    for (int t = 0; t <= gantt_end; t++) {
+        if (gantt[t] == 0) {
+            printf("|%*s", width - 1, "Idle");
         }
+        else if (gantt_io[t]) {
+            char buffer[width + 1];
+            snprintf(buffer, sizeof(buffer), "P%d(I/O)", gantt[t]);
+            printf("| %*s", width - 2, buffer);
+        }
+        else {
+            char buffer[width + 1];
+            snprintf(buffer, sizeof(buffer), "P%d", gantt[t]);
+            printf("| %*s", width - 2, buffer);
+        }
+    }
     printf("|\n");
 }
 
@@ -237,6 +633,5 @@ void evaluation() {
     double average_waiting_time = (double)total_waiting_time / process_count;
     double average_turnaround_time = (double)total_turnaround_time / process_count;
 
-    printf("\nEvaluation: Average waiting time = %.2f, Average turnaround time = %.2f\n", 
-        average_waiting_time, average_turnaround_time);
+    printf("\nEvaluation: Average waiting time = %.2f, Average turnaround time = %.2f\n", average_waiting_time, average_turnaround_time);
 }
